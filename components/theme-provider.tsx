3c837e8294c8d08c76b018e6539d0c6710f91522
Gracias Kasongo <graciaskas96@gmail.com>
2025-09-23 19:49:07 +0200

    updated

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,36 +1,30 @@
-'use client'
+"use client";
 
-import * as React from 'react'
-import { useEffect, useState } from 'react'
+import * as React from "react";
+import { useEffect, useState } from "react";
 import {
   ThemeProvider as NextThemesProvider,
   type ThemeProviderProps,
-} from 'next-themes'
-import { I18nextProvider } from 'react-i18next'
-import '../lib/i18n'
-import i18n from '../lib/i18n'
+} from "next-themes";
+import { I18nextProvider } from "react-i18next";
+import "../lib/i18n";
+import i18n from "../lib/i18n";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  const [isClient, setIsClient] = useState(false)
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
-    setIsClient(true)
+    setIsClient(true);
     // Load saved language preference
-    const savedLanguage = localStorage.getItem('preferred-language')
+    const savedLanguage = localStorage.getItem("preferred-language");
     if (savedLanguage && savedLanguage !== i18n.language) {
-      i18n.changeLanguage(savedLanguage)
+      i18n.changeLanguage(savedLanguage);
     }
-  }, [])
-
-  if (!isClient) {
-    return <div>Loading...</div>
-  }
+  }, []);
 
   return (
     <NextThemesProvider {...props}>
-      <I18nextProvider i18n={i18n}>
-        {children}
-      </I18nextProvider>
+      <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
     </NextThemesProvider>
-  )
+  );
 }
